2df274ab1b7f7ac9d71de5ebfe2500a5ae9d3eac
Mason Clarke <maclarkegdci@gmail.com>
2022-11-22 13:05:52 -0500

    Updated function name
    Updated function name for getting all the warehouses from the database

--- controllers/warehouseController.js
+++ controllers/warehouseController.js
@@ -1,7 +1,7 @@
 const knexConfig = require('../knexfile');
 const db = require('knex')(knexConfig);
 
-const getAllAsync = async (_req, res) => {
+const getAllWarehouses = async (_req, res) => {
   const warehouseData = await db('warehouses');
   res.status(200).json(warehouseData);
 };
