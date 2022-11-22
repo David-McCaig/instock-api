7883edfab1b8d965e59f04b2de51631df78a3d6e
cang <ho.kimcang@gmail.com>
2022-11-22 17:33:23 -0500

    add warehouse verification logic in getWarehouseInventories function

--- controllers/warehouseController.js
+++ controllers/warehouseController.js
@@ -6,20 +6,74 @@ const getAllWarehouses = async (_req, res) => {
   res.status(200).json(warehouseData);
 };
 
+// const getWarehouseInventories = async (req, res) => {
+//   try {
+//     const warehouseInventoriesData = await db("inventories")
+//       .where({ warehouse_id: req.params.id })
+//       .select("*");
+
+//     if (!warehouseInventoriesData.length) {
+//       res.status(404).json({ message: "Warehouse not found!" });
+//     } else {
+//       console.log("OK");
+//       res.status(200).json(warehouseInventoriesData);
+//     }
+
+//   } catch (error) {
+//     res.status(500).json({error: error});
+//   }
+// };
+
 const getWarehouseInventories = (req, res) => {
-  db("inventories")
-    .where({
-      warehouse_id: req.params.id,
-    })
+  db("warehouses")
     .select("*")
+    .from("warehouses")
+    .where({ id: req.params.id })
     .then((data) => {
-      if (!data.length)
-        res.status(404).json({ message: "Warehouse not found!" });
-      else res.status(200).json(data);
+        //Handle no id found
+        if (!data.length) {
+            res.status(404).json({message: "Warehouse doesn't exist!"});
+        }
+        else {
+            console.log(data[0].id)
+            return data[0].id
+        }
     })
-    .catch(() => {
-      res.status(500).send("Error getting warehouses");
+    .then((data) => {
+        db("inventories")
+            .where({
+              warehouse_id: data,
+            })
+            .select("*")
+            .then((data) => {
+              if (!data.length)
+                res.status(404).json({ message: "Warehouse empty!" });
+              else {
+                console.log("OK");
+                res.status(200).json(data);
+              }
+            })
+            .catch(() => {
+              res.status(500).send("Error getting warehouses");
+            });
     });
+
+  //   db("inventories")
+  //     .where({
+  //       warehouse_id: req.params.id,
+  //     })
+  //     .select("*")
+  //     .then((data) => {
+  //       if (!data.length)
+  //         res.status(404).json({ message: "Warehouse not found!" });
+  //       else {
+  //         console.log("OK");
+  //         res.status(200).json(data);
+  //       }
+  //     })
+  //     .catch(() => {
+  //       res.status(500).send("Error getting warehouses");
+  //     });
 };
 
 module.exports = {
