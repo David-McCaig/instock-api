029197a8cc14f01e5ff878bad0d0de3b8b54cb23
cang <ho.kimcang@gmail.com>
2022-11-23 16:59:13 -0500

    add validation for item quantity, item status corresponding to quantity, duplicate item in warehouse to addInventoryItem

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -11,21 +11,30 @@ const addInventoryItem = async (req, res) => {
     !req.body.description ||
     !req.body.category ||
     !req.body.status ||
-    req.body.quantity < 0
+    !req.body.quantity
   ) {
     return res.status(400).json({
       message:
         "Please make sure to provide warehouse, item name, description, category, status and quantity fields in your request.",
     });
   }
+  //Validate item quantity to not be less than 0
+  if (req.body.quantity < 0) {
+    return res.status(400).json({
+      message: "Please ensure that stock is not a negative number!",
+    });
+  }
+  //Validate item status and corresponding quantity
+  if (
+    (req.body.status === "In Stock" && req.body.quantity === 0) ||
+    (req.body.status === "Out of Stock" && req.body.quantity !== 0)
+  ) {
+    return res.status(400).json({
+      message: "Please ensure that stock status matches quantity!",
+    });
+  }
 
   try {
-    //Create inventory item
-    const inventoryItem = {
-      ...req.body,
-      id: uuid.v4(),
-    }
-
     //Check if warehouse exists
     const foundWarehouse = await db("warehouses").where({
       id: req.body.warehouse_id,
@@ -33,14 +42,27 @@ const addInventoryItem = async (req, res) => {
     if (!foundWarehouse.length) {
       return res.status(404).json({ message: "Warehouse doesn't exist!" });
     }
+    //Check if item exists in same warehouse
+    const duplicateItem = await db("inventories").where({
+      item_name: req.body.item_name,
+      warehouse_id: req.body.warehouse_id,
+    });
+    if (duplicateItem.length) {
+      return res
+        .status(404)
+        .json({ message: "Item already exists in warehouse, please edit!" });
+    }
 
-    //Check if item exists
+    //Create inventory item
+    const inventoryItem = {
+      ...req.body,
+      id: uuid.v4(),
+    };
 
     //Insert into inventory list
     await db("inventories").insert(inventoryItem);
     //Successful add
     res.status(201).json(inventoryItem);
-
   } catch (error) {
     console.log("error!");
     res.status(500).json({ error: error });
