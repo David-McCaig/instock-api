2bfdf3e98235a573d9341ad7407dab7091bab123
David-McCaig <davidmccaig1@gmail.com>
2022-11-24 10:04:29 -0500

    update configue settings

--- knexfile.js
+++ knexfile.js
@@ -2,15 +2,14 @@ require("dotenv").config();
 // Update with your config settings.
 
 const {
-  DB_LOCAL_DBNAME,
   DB_LOCAL_USER,
-  DB_LOCAL_PASSWORD
+  DB_LOCAL_PASSWORD,
+  DB_LOCAL_DBNAME
 } = process.env;
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
 module.exports = {
-
   client: "mysql2",
   connection: {
     host: "127.0.0.1",
